ddf8d3459b9f46231888b875e02d3077523cee08
toniraga <eragatony@gmail.com>
2021-05-07 01:35:57 +0100

    feature: created product summary component

---
+++ src/components/products/ProductSummaryCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <h3>{{product.name}}</h3>
+
+    <h5 class="price">Price: ${{product.price.toFixed(2)}}</h5>
+    <p class="description">Decription: {{description}}</p>
+    <p class="text-muted">{{product.category}}</p>
+    <button class="view-product-button">View</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  computed: {
+    description() {
+      return this.product.description.substring(0, 150)
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    width: 80%;
+    margin: 10%;
+    padding: 10px;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: 0 0 5px grey;
+
+    h5.price {
+      color: gray;
+    }
+
+    p.description {
+      font-size: .85rem;
+    }
+
+    p.text-muted {
+      color: gray;
+    }
+
+    button.view-product-button {
+      padding: 10px;
+      background-color: rgb(79, 160, 187);
+      border: none;
+      color: white;
+      font-weight: bold;
+      font-size: 1.15rem;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  }
+
+  @media (min-width: 500px) {
+    .card {
+      width: 350px;
+      margin: 10px;
+    }
+  }
+</style>
---
+++ src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+	{
+		id: 1,
+		name: 'Big Johns Popeye',
+		price: 12.0,
+		category: 'Footwear',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est voluptatum dolorem, iste sed temporibus.`,
+	},
+	{
+		id: 2,
+		name: 'Big Johns Popeye',
+		price: 12.0,
+		category: 'Footwear',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est voluptatum dolorem, iste sed temporibus.`,
+	},
+	{
+		id: 3,
+		name: 'Big Johns Popeye',
+		price: 12.0,
+		category: 'Footwear',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est voluptatum dolorem, iste sed temporibus.`,
+	},
+	{
+		id: 4,
+		name: 'Big Johns Popeye',
+		price: 12.0,
+		category: 'Footwear',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est voluptatum dolorem, iste sed temporibus.`,
+	},
+	{
+		id: 5,
+		name: 'Big Johns Popeye',
+		price: 12.0,
+		category: 'Footwear',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est voluptatum dolorem, iste sed temporibus.`,
+	},
+];
+
+export default items;
